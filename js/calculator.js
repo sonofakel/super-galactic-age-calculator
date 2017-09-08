ba710c4054487b01c229a198c4562781184faa06
Charlie Kelson <charlie.m.kelson@gmail.com>
2017-09-08 09:34:05 -0700

    working on spec 2.

--- js/calculator.js
+++ js/calculator.js
@@ -3,9 +3,17 @@ constructor () {
 }
 
 ageInSeconds(age) {
-  let secondsInYear = (18 * 31536000);
+  let secondsInYear = (age * 31536000);
   return secondsInYear;
 }
 
+secondsBetweenTwoDates(date1,date2) {
+  let a = moment(date1);
+  let b = moment(date2);
+  let result = a.diff(b, 'seconds');
 
-};
+  return result;
+
+}
+
+}
--- spec/calculator-spec.js
+++ spec/calculator-spec.js
@@ -3,8 +3,14 @@ import { Calculator } from './../js/calculator.js';
 describe('CalculatorTests', function() {
 
   it('should calculate the age of someone into seconds', function() {
-    var calculator = new Calculator()
+    var calculator = new Calculator();
     expect(calculator.ageInSeconds(18)).toEqual(567648000);
+
+  });
+
+  it('should calculate the seconds between two dates', function() {
+    var calculator = new Calculator();
+    expect(calculator.secondsBetweenTwoDates([2007, 0, 29],[2007, 0, 28])).toEqual(86400);
 
   });
 
